003e90a27230c7a8ba46cf3ff66a3e3c024322bd
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-14 18:38:38 -0300

    Refatoração do 4/1 bonus_3.js

--- 4-2/bonus_3.js
+++ 4-2/bonus_3.js
@@ -3,14 +3,28 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let multipliedNumbers = [];
 
-for (i = 0; i < numbers.length; i += 1) {
-    multipliedNumbers.push(numbers[i] * numbers[i + 1]);
-
-    if (i === numbers.length - 1) {
-        multipliedNumbers.pop();
-        
+for (i = 0; i < numbers.length; i += 1) {   
+    if (i !== numbers.length - 1) {
+        multipliedNumbers.push(numbers[i] * numbers[i + 1]);
+    } else {
         multipliedNumbers.push(numbers[i] * 2);
     }
-}
+}   
 
 console.log("Novo array: " + multipliedNumbers);
+
+// 1st try com GAMBIARRA
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let multipliedNumbers = [];
+
+// for (i = 0; i < numbers.length; i += 1) {
+//     multipliedNumbers.push(numbers[i] * numbers[i + 1]);
+
+//     if (i === numbers.length - 1) {
+//         multipliedNumbers.pop();
+
+//         multipliedNumbers.push(numbers[i] * 2);
+//     }
+// }
+
+// console.log("Novo array: " + multipliedNumbers);
