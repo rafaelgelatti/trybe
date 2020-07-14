b4d223899ff1ab5e6542b1662d7ceca120ac4ff6
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-14 18:43:18 -0300

    Refactoring 4-2/bonus_3.js and adding draft.js for drafts

--- 4-2/bonus_3.js
+++ 4-2/bonus_3.js
@@ -1,4 +1,7 @@
-//3 - Agora você irá criar um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (valor correspondente) e 9 (próximo valor). Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push.
+//3 - Agora você irá criar um novo array a partir do array numbers, sem perdê-lo.
+//Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo.
+//Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (valor correspondente) e 9 (próximo valor).
+//Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let multipliedNumbers = [];
@@ -12,19 +15,3 @@ for (i = 0; i < numbers.length; i += 1) {
 }   
 
 console.log("Novo array: " + multipliedNumbers);
-
-// 1st try com GAMBIARRA
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let multipliedNumbers = [];
-
-// for (i = 0; i < numbers.length; i += 1) {
-//     multipliedNumbers.push(numbers[i] * numbers[i + 1]);
-
-//     if (i === numbers.length - 1) {
-//         multipliedNumbers.pop();
-
-//         multipliedNumbers.push(numbers[i] * 2);
-//     }
-// }
-
-// console.log("Novo array: " + multipliedNumbers);
