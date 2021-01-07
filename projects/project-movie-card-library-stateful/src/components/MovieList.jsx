d201aca800316ae0921280e33fcf14a31e2ca20e
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-07 19:17:04 -0300

    moviecard library stateful project

---
+++ projects/project-movie-card-library-stateful/src/components/MovieList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div data-testid="movie-list" className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ),
+};
+
+MovieList.defaultProps = {
+  movies: PropTypes.shape({
+    title: '',
+    subtitle: '',
+    storyline: '',
+    rating: 0,
+    imagePath: '',
+  }),
+};
+
+export default MovieList;
