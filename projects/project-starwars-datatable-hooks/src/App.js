0059a9371d6b382b9acf7136695028464827a37a
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-07 19:20:18 -0300

    starwars data table hooks project

---
+++ projects/project-starwars-datatable-hooks/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PlanetContextProvider from './contexts/PlanetContext';
+import FilterContextProvider from './contexts/FilterContext';
+import Navbar from './components/Navbar';
+import PlanetsTable from './components/PlanetsTable';
+import Filters from './components/Filters';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <PlanetContextProvider>
+        <FilterContextProvider>
+          <Filters />
+          <PlanetsTable />
+        </FilterContextProvider>
+      </PlanetContextProvider>
+    </div>
+  );
+}
+
+export default App;
